524b1febeca73d7ad971193846ade2cae7b17f12
haidyomar645-star <haidyomar645@gmail.com>
2026-03-27 17:36:54 +0200

    edit ingest.py

--- ingest.py
+++ ingest.py
@@ -1,10 +1,14 @@
 import sys
 import pandas as pd
+import os  
 
 file_path = sys.argv[1]
 
 df = pd.read_csv(file_path)
 
-df.to_csv("data_raw.csv", index=False)
+output_path = "data_raw.csv"
+df.to_csv(output_path, index=False)
 
 print("Data saved as data_raw.csv")
+
+os.system(f"py preprocess.py {output_path}")
